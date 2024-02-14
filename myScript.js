99cd42edd5228bb09095cbaaeb0a6dd72d019901
Dennis <dennis.barth13@gmail.com>
2024-02-14 15:42:17 +0100

    picovinky

--- myScript.js
+++ myScript.js
@@ -5,12 +5,26 @@ const employe = {
     proffesion:"Programmer"
     };
 
+    let age=4;
+    
+    const name=Dennis;
+    
+
+
     document.write(employe.fname);
     document.write("<br>");
     document.write(employe.lname);
     document.write("<br>");
     document.write(employe.age);
     document.write("<br>");
-    document.write(employe.proffesion);    
+    document.write(employe.proffesion);  
+    document.write("<br>");
+    document.write("<br>");
+
+
+    document.write(anem);
+
+
+
 
 
